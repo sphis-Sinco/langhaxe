e799d8ce84cf5a910173236597de2f9f7b74db17
IdealistCat <mattallan.h@gmail.com>
2024-12-24 23:27:55 -0600

    language files (json basically)

--- langhaxe/Language.hx
+++ langhaxe/Language.hx
@@ -2,6 +2,6 @@ package langhaxe;
 
 class Language
 {
-	public static var LANGUAGE_FILE_EXTENSION:String = 'lang';
+	public static var LANGUAGE_FILE_EXTENSION:String = 'langhx';
 	public static var LANGUAGE_FOLDER:String = 'translations';
 }
