9e798792dab36b0c5354d2cbc33d5bfbd20aa6a1
IdealistCat <mattallan.h@gmail.com>
2024-12-24 23:54:17 -0600

    first version + url fix

--- example/source/PlayState.hx
+++ example/source/PlayState.hx
@@ -1,16 +1,43 @@
 package;
 
+import langhaxe.LangHaxe;
+import flixel.FlxG;
+import langhaxe.Language;
+import flixel.text.FlxText;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var cooltext:FlxText = new FlxText(0,0,0,"Hello World", 32);
+	var lang:String = 'es';
+	var langFile:LangHaxe;
+
 	override public function create()
 	{
+		langFile = Language.readLanguageFile(lang);
+
+		add(cooltext);
+		cooltext.alignment = CENTER;
+		updateText();
+
 		super.create();
 	}
 
 	override public function update(elapsed:Float)
 	{
+		if (FlxG.keys.justReleased.ANY)
+			updateText();
+
 		super.update(elapsed);
+	}
+
+	function updateText() {
+		if (lang == 'es') lang = 'en';
+		else lang = 'es';
+
+		var newtext:String = langFile.phrases.get('hello_world');
+		newtext += '\n' + langFile.phrases.get('how_are_you');
+		cooltext.text = newtext.toString();
+		cooltext.screenCenter();
 	}
 }
